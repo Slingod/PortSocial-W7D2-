b668435340b4705ee3fe5cc617cbaa14f522bfb7
slingod <slingo.drisca1@gmail.com>
2025-06-09 10:05:36 +0200

    Add EnvelopeAnimation component and integrate it into Contact page; update styles and dependencies

---
+++ src/components/EnvelopeAnimation.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './EnvelopeAnimation.scss';
+
+export default function EnvelopeAnimation() {
+  return (
+    <a href="mailto:slingo.drisca@gmail.com" className="letter-image-link">
+      <div className="letter-image">
+        <div className="animated-mail">
+          <div className="back-fold"></div>
+          <div className="letter">
+            <div className="letter-border"></div>
+            <div className="letter-title"></div>
+            <div className="letter-context"></div>
+            <div className="letter-stamp"></div>
+          </div>
+          <div className="top-fold"></div>
+          <div className="body"></div>
+          <div className="left-fold"></div>
+        </div>
+        <div className="shadow"></div>
+      </div>
+    </a>
+  );
+}
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import EnvelopeAnimation from '../components/EnvelopeAnimation';
+import './Contact.scss';  // si vous avez des styles spécifiques
 
 export default function Contact() {
   return (
     <div className="page page--contact">
       <h1 className="page__title">Contact</h1>
+
+      {/* Animation enveloppe */}
+      <EnvelopeAnimation />
+
       <p className="page__text">
-        N’hésitez pas à me contacter à l’adresse suivante : <br />
-        <a href="mailto:slingo.drisca@gmail.com">slingo.drisca@gmail.com</a>
+        N’hésitez pas à me contacter à l’adresse suivante :<br/>
+        <a href="mailto:slingo.drisca@gmail.com">
+          slingo.drisca@gmail.com
+        </a>
       </p>
     </div>
   );
